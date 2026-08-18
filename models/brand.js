c5cf6e353ba3cfe2943920e82a9cc88cec14b29f
roaayala <roaayala.ahm@gmail.com>
2026-08-18 22:31:01 +0800

    models/brand: done

--- models/brand.js
+++ models/brand.js
@@ -1,7 +1,11 @@
-const Brand = ({ id, name }) => {
+const Brand = ({ id, name, productIds, products }) => {
   return {
     id: id || crypto.randomUUID(),
     name: name || "No Brand",
+
+    productIds: productIds || [],
+
+    products: products || [],
   };
 };
 
