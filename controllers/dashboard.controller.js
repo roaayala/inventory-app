a7c5df35baf368906550f3f6f147f388275ff792
roaayala <roaayala.ahm@gmail.com>
2026-08-22 13:53:45 +0800

    dashboard/index: WIP

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -1,6 +1,12 @@
 import * as dashboardService from "../services/dashboard.service.js";
+import { stringifyPrice } from "../utils/helpers.js";
 
 export const getProducts = async (_req, res) => {
   const products = await dashboardService.getProducts();
-  res.send(products);
+
+  res.render("dashboard/index", {
+    title: "Dashboard",
+    products,
+    stringifyPrice,
+  });
 };
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,2 +1,8 @@
 export const newPlaceholders = (arr) =>
   arr.map((_, index) => `$${index + 1}`).join(", ");
+
+export const stringifyPrice = (price) => {
+  if (price === null || price === undefined || isNaN(price)) return "0";
+
+  return new Intl.NumberFormat("id-ID").format(price);
+};
