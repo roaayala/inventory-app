5c465607f35998306668b978186f01aeaef02f55
roaayala <roaayala.ahm@gmail.com>
2026-08-25 14:19:54 +0800

    views/dashobard/index: sidebar WIP

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -3,7 +3,7 @@ import * as categoryService from "../services/category.service.js";
 import * as brandService from "../services/brand.service.js";
 import { buildCategoryTree, stringifyPrice } from "../utils/helpers.js";
 
-export const renderDashboard = async (_req, res) => {
+export const renderDashboard = async (req, res) => {
   const products = await productService.getProducts();
   const categories = buildCategoryTree(await categoryService.getCategories());
   const brands = await brandService.getBrands();
